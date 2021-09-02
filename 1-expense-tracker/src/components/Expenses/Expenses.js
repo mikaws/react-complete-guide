164df789eee13a830793cc5a3f1326c651a659cc
MichaelHenrique <michaelhenriqued@gmail.com>
2021-09-01 23:41:23 -0300

    add Chart component

--- 1-expense-tracker/src/components/Expenses/Expenses.js
+++ 1-expense-tracker/src/components/Expenses/Expenses.js
@@ -3,6 +3,8 @@ import './Expenses.css'
 import ExpensesFilter from './ExpensesFilter';
 import React, {useState} from 'react';
 import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
+import './Expenses.css';
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState('2020');
@@ -17,7 +19,10 @@ const Expenses = (props) => {
   return (
       <li>
         <Card className="expenses">
-          <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
+          <ExpensesFilter
+            selected={filteredYear}
+            onChangeFilter={filterChangeHandler}/>
+          <ExpensesChart expenses={filteredExpenses}/>
           <ExpensesList items={filteredExpenses}/>
         </Card>
       </li>
