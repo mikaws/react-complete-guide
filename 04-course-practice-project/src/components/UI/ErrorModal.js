5a8ccce8073af1bce9a6ea3c7319dc67ebe91a1d
mikaws <michaelhenriqued@gmail.com>
2022-01-05 22:23:33 -0300

    append the project files into repository

--- 04-course-practice-project/src/components/UI/ErrorModal.js
+++ 04-course-practice-project/src/components/UI/ErrorModal.js
@@ -1,23 +1,26 @@
-import Card from './Card'
-import Button from './Button'
-import classes from './ErrorModal.module.css'
+import React from 'react';
 
-const ErrorModal = props => {
-    return(
-        <div className={classes.backdrop} onClick={props.onConfirm}>
-            <Card className={classes.modal}>
-                <header className={classes.header}>
-                    <h2>{props.title}</h2>
-                </header>
-                <div className={classes.content}>
-                    <p>{props.message}</p>
-                </div>
-                <footer className={classes.actions}>
-                    <Button onClick={props.onConfirm}>Okay</Button>
-                </footer>
-            </Card>
+import Card from './Card';
+import Button from './Button';
+import classes from './ErrorModal.module.css';
+
+const ErrorModal = (props) => {
+  return (
+    <div>
+      <div className={classes.backdrop} onClick={props.onConfirm} />
+      <Card className={classes.modal}>
+        <header className={classes.header}>
+          <h2>{props.title}</h2>
+        </header>
+        <div className={classes.content}>
+          <p>{props.message}</p>
         </div>
-    )
-    
-}
-export default ErrorModal
+        <footer className={classes.actions}>
+          <Button onClick={props.onConfirm}>Okay</Button>
+        </footer>
+      </Card>
+    </div>
+  );
+};
+
+export default ErrorModal;
