a5bd17f9332ee3ce64d6d025248e72fc42005207
mikaws <michaelhenriqued@gmail.com>
2022-02-15 19:51:39 -0300

    feat: add context hook to logon

--- 05-project-with-hooks/src/components/Login/Login.js
+++ 05-project-with-hooks/src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect, useReducer } from 'react';
+import React, { useState, useEffect, useReducer, useContext } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
+import AuthContext from '../../store/auth-context';
 
 const emailReducer = (state, action) => {
   if (action.type === 'USER_INPUT') {
@@ -52,6 +53,8 @@ const Login = (props) => {
   const { isValid: emailIsValid } = emailState
   const { isValid: passwordIsValid } = emailState
 
+  const authCtx = useContext(AuthContext)
+
   useEffect(() => {
     const identifier = setTimeout(() => {
       setFormIsValid(
@@ -80,7 +83,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    authCtx.onLogin(emailState.value, passwordState.value);
   };
 
   return (
